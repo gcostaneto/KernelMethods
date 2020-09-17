9667f2c93124e1d6ce01f519885a8b83d5aac465
Germano Costa Neto <germano.cneto@gmail.com>
2020-09-17 20:02:28 -0300

    Update Dominance_Matrix.R

--- Dominance_Matrix.R
+++ Dominance_Matrix.R
@@ -8,12 +8,6 @@ Dominance <-function(M){
     
     S <- ((M==2)*1) * - rep(2*(1-p)^2, each=N) + ((M==1)*1) * rep(2*p*(1-p), each=N) + ((M==0)*1) * (-rep(2*p^2, each=N))
     
-    WWl <- w %*% t(w)
-    Ga <- WWl/(sum(diag(WWl))/N) + diag(1e-6, nrow(WWl))
-    
-    SSl <- S %*% t(S)
-    Gd <- SSl/(sum(diag(SSl))/N)
-    
     return(S)
   }
   
