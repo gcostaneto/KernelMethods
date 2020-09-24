b009f1a4d7674202d576e97715f51864f3b9f7cf
Germano Costa Neto <germano.cneto@gmail.com>
2020-09-24 15:04:49 -0300

    Update Dominance_Matrix.R

--- Dominance_Matrix.R
+++ Dominance_Matrix.R
@@ -1,15 +1,9 @@
 Dominance <-function(M){
+  # from SnpReady
   N <- nrow(M)
   m <- ncol(M)
   p <- colMeans(M)/2
-  
-  WWG <- function(M, p){
-    w <- scale(x = M, center = T, scale = F)
-    
-    S <- ((M==2)*1) * - rep(2*(1-p)^2, each=N) + ((M==1)*1) * rep(2*p*(1-p), each=N) + ((M==0)*1) * (-rep(2*p^2, each=N))
-    
-    return(S)
-  }
-  
-  return(WWG(M=M,p=p))
+  w <- scale(x = M, center = T, scale = F)
+  S <- ((M==2)*1) * - rep(2*(1-p)^2, each=N) + ((M==1)*1) * rep(2*p*(1-p), each=N) + ((M==0)*1) * (-rep(2*p^2, each=N))
+ return(S) 
 }
