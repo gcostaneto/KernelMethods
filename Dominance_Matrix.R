cb8e7942be5366edd2bd06a33f173111976f4125
Germano Costa Neto <germano.cneto@gmail.com>
2020-09-24 15:30:33 -0300

    Update Dominance_Matrix.R

--- Dominance_Matrix.R
+++ Dominance_Matrix.R
@@ -6,7 +6,6 @@ S.matrix <-function(M){
   N <- nrow(M)
   m <- ncol(M)
   p <- colMeans(M)/2
-  w <- scale(x = M, center = T, scale = F)
   S <- ((M==2)*1) * - rep(2*(1-p)^2, each=N) + ((M==1)*1) * rep(2*p*(1-p), each=N) + ((M==0)*1) * (-rep(2*p^2, each=N))
  return(S) 
 }
